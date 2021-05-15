01f4d741abb3b8e2a41b411e25ab907921d32949
wakazoku <wakazoku@gmail.com>
2021-05-15 22:06:23 +0900

    Initial Commit

---
+++ Swift6Button1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift6Button1
+//
+//  Created by zoku on 2021/05/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
